5fdafd19d318e8c3cbd4ae56c982ec27c6ea2112
MagnusJs <dinhbach3062001@gmail.com>
2022-09-19 09:41:12 +0700

    Back-end Employee CRUD

---
+++ src/main/java/com/magnus/employeemanagement/model/Employee.java
@@ -0,0 +1,15 @@
+package com.magnus.employeemanagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailID;
+}
---
+++ src/main/java/com/magnus/employeemanagement/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.magnus.employeemanagement.repository;
+
+import com.magnus.employeemanagement.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
